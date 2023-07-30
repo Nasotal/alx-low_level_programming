65191e70693323d10b6d91a9dd689a5384581586
Nasotal <tobioguntuasee@gmail.com>
2023-07-30 01:44:48 -0700

    Dog Update

---
+++ 1-init_dog.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * init_dog - initializes the dog struct
+ * @dg: pointer to struct dog
+ * @name: name
+ * @age: age
+ * @owner: owner
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (dg == NULL)
+		dg = malloc(sizeof(struct dog));
+	dg->name = name;
+	dg->age = age;
+	dg->owner = owner;
+}
---
+++ 2-print_dog.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * print_dog - prints a dog struct
+ * @dg: struct dog to print
+ */
+void print_dog(struct dog *d)
+{
+	if (dg == NULL)
+		return;
+
+	if (dg->name == NULL)
+		dg->name = "(nil)";
+	if (dg->owner == NULL)
+		dg->owner = "(nil)";
+
+	printf("Name: %s\nAge: %f\nOwner: %s\n", dg->name, dg->age, dg->owner);
+}
