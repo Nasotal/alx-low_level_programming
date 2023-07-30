8b8982f13a1ee70f30fc0aa0fff850ac3193067a
Nasotal <tobioguntuasee@gmail.com>
2023-07-30 02:17:31 -0700

    update

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,4 +1,4 @@
-ifndef DOG_H
+#ifndef DOG_H
 #define DOG_H
 /**
  * struct dog - About a particular dog
@@ -7,13 +7,13 @@ ifndef DOG_H
  * @age: Dog's age
  * @owner: Name of the owner
  * Description: Longer description
- * 
+ *
  */
 struct dog
 {
-        char *name;
-        float age;
-        char *owner;
+	char *name;
+	float age;
+	char *owner;
 };
 
 /**
