dd30ff610e2e7cb7769d3e944c288fb9f09733ea
Nasotal <tobioguntuasee@gmail.com>
2023-07-31 13:38:27 -0700

    Preprocessor

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,11 @@
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
+
+/*
+ * a function-like macro ABS(y)
+ * that computes the absolute value of a number y
+ */
+
+#define ABS(y) ((y) < (0) ? -(y) : (y))
+
+#endif /* _FUNCTION_LIKE_MACRO_H */
