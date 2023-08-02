be97bba8f8bbc046158a9a1adceb38a874623f7b
Nasotal <tobioguntuasee@gmail.com>
2023-08-02 00:01:46 -0700

    updated

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -6,7 +6,7 @@ void __attribute__((constructor)) prev(void);
  * prev - a function that prints a string before the
  * main function is executed.
  */
-void bfor(void)
+void prev(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
